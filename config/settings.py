8281a58ff8f1c7cf9fd69886b82a0be3a3132823
badgersky <szymonek.ptasznik@gmail.com>
2023-10-15 22:30:22 +0200

    update urls and settings

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-import _
+from config import _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # third party
-    'debug-toolbar',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
