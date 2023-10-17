eb89242aa8c50689430d2f23f7754466b1e925e1
badgersky <szymonek.ptasznik@gmail.com>
2023-10-17 22:03:56 +0200

    created account model, migrated database

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     # third party
     'debug_toolbar',
     'bootstrap5',
+    
+    # my apps
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +129,5 @@ STATIC_ROOT = 'staticfiles/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.Account'
